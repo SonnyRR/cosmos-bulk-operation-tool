888f8f81c32ecb980b533052fcb19dde5f8ffb57
Vasil Kotsev <9307969+SonnyRR@users.noreply.github.com>
2025-09-20 15:54:52 +0300

    Extend Az Cosmos DB fixture

--- Cosmos.BulkOperation.Tests/BulkInsertionTests.cs
+++ Cosmos.BulkOperation.Tests/BulkInsertionTests.cs
@@ -1,15 +1,26 @@
-using Cosmos.BulkOperation.CLI;
+using Shouldly;
 
 namespace Cosmos.BulkOperation.Tests;
 
 public class BulkInsertionTests : IClassFixture<CosmosDatabaseFixture>
 {
+    private readonly CosmosDatabaseFixture fixture;
+
+    public BulkInsertionTests(CosmosDatabaseFixture fixture)
+    {
+        this.fixture = fixture;
+    }
+
     [Fact]
     public async Task Test1()
     {
         // Arrange
         using var a = Utils.UseEnvironment("Testing");
 
-        await Program.Main(null);
+        await this.fixture.GetClient().CreateDatabaseIfNotExistsAsync("sandbox");
+        // Act
+
+        // Assert
+        true.ShouldBe(true);
     }
 }
